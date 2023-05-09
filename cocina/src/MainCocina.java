81db740e04314f0de01963c788e0de4ed5195b39
Alexander Esquivel Barrientos <esq.bar.alex@gmail.com>
2023-05-08 19:52:36 -0600

    Repo Init

---
+++ cocina/src/MainCocina.java
@@ -0,0 +1,5 @@
+public class MainCocina {
+    public static void main(String[] args) {
+        System.out.println("MainCocina");
+    }
+}
---
+++ salon/src/MainSalon.java
@@ -0,0 +1,5 @@
+public class MainSalon {
+    public static void main(String[] args) {
+        System.out.println("MainSalon");
+    }
+}
